94a81b54ee3f97ac1a0e49b77de4ae1e32b6ff97
Giancarlo Haack Albano <gian418@gmail.com>
2024-03-07 22:25:32 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/haack/jokenpo_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.haack.jokenpo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Jogo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "JokenPo"
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App:",
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
